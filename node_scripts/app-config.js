534f4fb98e7295e595f95ef1991ba6f80eae94b5
chrismurdy <88800421+chrismurdy@users.noreply.github.com>
2021-08-11 17:12:30 -0400

    Delete node_scripts directory

--- node_scripts/app-config.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs-extra');
-
-if(process.argv[2] === 'media'){
-    fs.copy('./data/appConfig.media.json', './dist/data/appConfig.json', function (err) { if (err)  console.error(err) });
-}
-
-if(process.argv[2] === 'takeover'){
-    fs.copy('./data/appConfig.takeover.json', './dist/data/appConfig.json', function (err) { if (err)  console.error(err) });
-}
